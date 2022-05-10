69e0c1a9e0918ff3c679d2de41f8d66356416aaf
sevichecc <sevicheee@outlook.com>
2022-05-10 18:51:59 +0800

    disable clearInput

--- src/components/ValidateForm.vue
+++ src/components/ValidateForm.vue
@@ -14,18 +14,18 @@ import { defineComponent, onUnmounted } from 'vue'
 import mitt from 'mitt'
 
 type ValidateFunc = () => boolean
-type ClearFunc = () => void
-type Events = {
-  'form-item-created': ValidateFunc
-  'form-item-clear': ClearFunc
-}
-export const emitter = mitt<Events>()
+// type ClearFunc = () => void
+// type Events = {
+//   'form-item-created': ValidateFunc
+//   // 'form-item-clear'?: ClearFunc
+// }
+export const emitter = mitt()
 
 export default defineComponent({
   emits: ['form-submit'],
   setup (props, context) {
     let funcArr: ValidateFunc [] = []
-    let clearArr: ClearFunc [] = []
+    // let clearArr: ClearFunc [] = []
 
     // 将监听得到的验证函数放到一个数组中
     const validateCallback = (func?: ValidateFunc) => {
@@ -34,26 +34,26 @@ export default defineComponent({
       }
     }
     // 用于清空输入框的函数数组
-    const clearCallback = (func: ClearFunc) => {
-      clearArr.push(func)
-    }
+    // const clearCallback = (func: ClearFunc) => {
+    //   clearArr.push(func)
+    // }
     // 循环验证数组内的函数
     const submitForm = () => {
       const result = funcArr.map(func => func()).every(result => result)
       context.emit('form-submit', result)
-      clearArr.map(func => func())
+      // clearArr.map(func => func())
     }
 
     // 添加监听
     emitter.on('form-item-created', validateCallback)
-    emitter.on('form-item-clear', clearCallback)
+    // emitter.on('form-item-clear', clearCallback)
 
     // 删除监听
     onUnmounted(() => {
       emitter.off('form-item-created', validateCallback)
-      emitter.off('form-item-clear', clearCallback)
+      // emitter.off('form-item-clear', clearCallback)
       funcArr = []
-      clearArr = []
+      // clearArr = []
     })
     return {
       submitForm
