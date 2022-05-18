225cdbf3a80b5986cc17c0a40404eb2bdaff13d7
sevichecc <sevicheee@outlook.com>
2022-05-18 21:29:47 +0800

    9-5

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,11 @@
       </div>
     </section>
     <h4 class="font-weight-bold text-center">发现精彩</h4>
+    <uploader action="/upload" :beforeUpload="beforeUpload" @file-uploaded="onFileUploaded">
+      <template #uploaded="dataProps">
+        <img :src="dataProps.uploadedData.data.url" width="500"/>
+      </template>
+    </uploader>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -19,20 +24,37 @@
 <script lang="ts">
 import { defineComponent, computed, onMounted } from 'vue'
 import { useStore } from 'vuex'
-import { GlobalDataProps } from '../store'
+import { GlobalDataProps, ResponseType, ImageProps } from '../store'
 import ColumnList from '../components/ColumnList.vue'
+import Uploader from '../components/Uploader.vue'
+import createMessage from '../components/createMessage'
 
 export default defineComponent({
   name: 'Home',
-  components: { ColumnList },
+  components: { ColumnList, Uploader },
   setup () {
     const store = useStore<GlobalDataProps>()
+
     onMounted(() => {
       store.dispatch('fetchColumns')
     })
+
     const list = computed(() => store.state.columns)
+
+    const beforeUpload = (file: File) => {
+      const isJPG = file.type === 'image/jpeg'
+      if (!isJPG) {
+        createMessage('上传图片只能是 JPG 格式!', 'error')
+      }
+      return isJPG
+    }
+    const onFileUploaded = (rawData: ResponseType<ImageProps>) => {
+      createMessage(`上传图片ID ${rawData.data._id}`, 'success')
+    }
     return {
-      list
+      list,
+      beforeUpload,
+      onFileUploaded
     }
   }
 })
