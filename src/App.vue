ec1eec1235494d9e37647799aa60d348f72082e2
sevichecc <91365763+Sevichecc@users.noreply.github.com>
2022-06-19 17:00:56 +0800

    add github link

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,7 @@
       <small>
         <ul class="list-inline mb-0">
           <li class="list-inline-item">© 2022 者也专栏</li>
-          <li class="list-inline-item">课程</li>
-          <li class="list-inline-item">文档</li>
-          <li class="list-inline-item">联系</li>
-          <li class="list-inline-item">更多</li>
+          <li class="list-inline-item"><a href="https://github.com/Sevichecc/zheye">Github</a></li>
         </ul>
       </small>
     </footer>
